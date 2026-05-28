9286e27996e1ab6ada8fb8e53eef070735bbf163
Vikas4983 <vikaskumarofficial2023@gmail.com>
2026-05-28 16:28:34 +0530

    fix changes

--- routes/web.php
+++ routes/web.php
@@ -15,11 +15,11 @@ use Illuminate\Support\Facades\Route;
 //     return view('auth.login');
 // });
 Route::get('/', function () {
-    return view('home');
-})->name('/');
-Route::get('login', function () {
     return view('auth.login');
-})->name('login');
+})->name('/');
+Route::get('register', function () {
+    return view('auth.login');
+})->name('register');
 Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified'])->group(function () {
     Route::get('/dashboard', function () {
         return view('dashboard');
@@ -44,4 +44,3 @@ Route::get('marriage-invitation', [WeddingController::class, 'view'])->name('vie
 Route::post('gallery', [ImageController::class, 'gallery'])->name('gallery.image');
 Route::view('emails.reminder-wedding-invitation', 'emails.reminder-wedding-invitation');
 
-Route::post('gallery', [ImageController::class, 'gallery'])->name('gallery.image');
